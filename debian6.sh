76599fa1e6ec948992dee7760151ab6cfc3fde06
Arie Wijayanto <me@kangarie.com>
2014-01-07 11:45:40 +0700

    Update debian6.sh

--- debian6.sh
+++ debian6.sh
@@ -65,6 +65,7 @@ service nginx restart
 wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/arieonline/autoscript/master/conf/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
+wget -O /etc/openvpn/1194.conf "https://raw.github.com/arieonline/autoscript/master/conf/1194.conf"
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
